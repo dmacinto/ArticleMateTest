87744704fff06a7cfee00b6b041be89dee17f7be
dmacinto <dmacinto@gmail.com>
2023-02-21 02:06:28 +0000

    webpacker and react installed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "apis#index"
 
-  get '/convert_to_speech/:id', to: 'pub_med#convert_to_speech'
+  get '/convert_to_speech/:id', to: 'api#convert_to_speech'
 
 end
